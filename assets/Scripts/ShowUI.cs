6a220933b1adc4eb817cb17674299960998adcb9
mdante97 <56942288+mdante97@users.noreply.github.com>
2019-10-31 20:55:44 -0400

    Add files via upload

---
+++ assets/Scripts/ShowUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject uiObject;
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    void OnTriggerEnter (Collider player)
+    {
+      if(player.gameObject.tag == "Player")
+      {
+        uiObject.SetActive(true);
+        gameObject.GetComponent<Renderer>().enabled = false;
+        StartCoroutine("WaitForSec");
+      }
+    }
+
+    IEnumerator WaitForSec()
+    {
+      yield return new WaitForSeconds(5);
+      Destroy(uiObject);
+      Destroy(gameObject);
+    }
+}
